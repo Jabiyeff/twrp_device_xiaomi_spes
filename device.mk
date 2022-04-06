c0d4ea2d2a48e97b90f41f042a26e89b317c15f7
boedhack99 <boedhack@gmail.com>
2022-04-07 00:49:42 +0700

    spes: Cleanup build target

--- device.mk
+++ device.mk
@@ -34,7 +34,8 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_OPTIONAL_vendor=true
 
 PRODUCT_PACKAGES += \
-    otapreopt_script
+    otapreopt_script \
+    checkpoint_gc
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
@@ -47,21 +48,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-
-# Display
-PRODUCT_PACKAGES += \
-    libdisplayconfig.qti \
-    libgralloc.qti \
-    libqdMetaData \
-    libqdMetaData.system \
-    libvulkan \
-    vendor.display.config@1.0 \
-    vendor.display.config@2.0 \
-    vendor.qti.hardware.display.composer@3.0
-
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
@@ -71,53 +57,15 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
-# Framework detect
-PRODUCT_PACKAGES += \
-    libqti_vndfwk_detect \
-    libvndfwk_detect_jni.qti
-
-# HIDL
-PRODUCT_PACKAGES += \
-    android.hidl.base@1.0
-
 # Init scripts
 PRODUCT_PACKAGES += \
     init.recovery.qcom.sh \
     init.recovery.qcom.rc
 
-# Input
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
-    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
-
-# Lights
-PRODUCT_PACKAGES += \
-    android.hardware.lights-service.qti
-
-# Media
-PRODUCT_PACKAGES += \
-    libavservices_minijail
-
-PRODUCT_PACKAGES += \
-    NotchBarKiller
-
-# Perf
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.2
-
-# Ramdisk
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.default:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/qcom-caf/bootctrl
-
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power-service.spes
 
 # Update engine
 PRODUCT_PACKAGES += \
@@ -127,10 +75,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
-
-# Vendor overlay
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/prebuilt/lib/,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)lib)
 
 PRODUCT_PACKAGES += \
     qcom_decrypt \
