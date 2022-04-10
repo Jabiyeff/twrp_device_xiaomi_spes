49c00e48413546ef7f5acf981b9f42914a75676f
Jabiyeff <cebiyevanar@gmail.com>
2022-04-10 16:10:35 +0400

    spes: Remove unused files and flags
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,9 +49,6 @@ TARGET_NO_BOOTLOADER := true
 # Build Hack
 BUILD_BROKEN_DUP_RULES := true
 
-# Filesystem
-TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
-
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
@@ -88,9 +85,6 @@ BOARD_KERNEL_CMDLINE += swiotlb=2048
 
 TARGET_FORCE_PREBUILT_KERNEL := true
 TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_CONFIG := vendor/bengal_defconfig
-TARGET_KERNEL_HEADERS := kernel/xiaomi/spes
-TARGET_KERNEL_SOURCE := kernel/xiaomi/spes
 
 # Metadata
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
