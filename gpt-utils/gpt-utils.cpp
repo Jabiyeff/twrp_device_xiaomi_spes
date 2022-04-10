be2b5fc11ed39248c5efd898b489583f346f7e03
Jabiyeff <cebiyevanar@gmail.com>
2022-04-10 16:35:17 +0400

    spes: gpt-utils: Fix build errors
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- gpt-utils/gpt-utils.cpp
+++ gpt-utils/gpt-utils.cpp
@@ -52,7 +52,7 @@
 
 
 #define LOG_TAG "gpt-utils"
-#include <cutils/log.h>
+#include <log/log.h>
 #include <cutils/properties.h>
 #include "gpt-utils.h"
 #include <zlib.h>
@@ -157,18 +157,11 @@ static int blk_rw(int fd, int rw, int64_t offset, uint8_t *buf, unsigned len)
     else
         r = read(fd, buf, len);
 
-    if (r < 0) {
+    if (r < 0)
         fprintf(stderr, "block dev %s failed: %s\n", rw ? "write" : "read",
                 strerror(errno));
-    } else {
-        if (rw) {
-            r = fsync(fd);
-            if (r < 0)
-                fprintf(stderr, "fsync failed: %s\n", strerror(errno));
-        } else {
-            r = 0;
-        }
-    }
+    else
+        r = 0;
 
     return r;
 }
