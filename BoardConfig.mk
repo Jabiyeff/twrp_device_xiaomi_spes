1ae76b77e98bec829e1d42f5c76d4feda7383b42
Jabiyeff <cebiyevanar@gmail.com>
2022-06-06 22:58:53 +0400

    spes: Set selinux to permissive
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,6 +78,7 @@ BOARD_KERNEL_CMDLINE += \
     androidboot.console=ttyMSM0 \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
+    androidboot.selinux=permissive \
     androidboot.usbcontroller=4e00000.dwc3 \
     console=ttyMSM0,115200n8 \
     earlycon=msm_geni_serial,0x4a90000 \
