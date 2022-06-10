c6a7254c34f84bb95fbb067e1798d4b8039e0997
Jabiyeff <cebiyevanar@gmail.com>
2022-06-10 23:04:47 +0400

    use ccache while building
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,10 @@
 export ALLOW_MISSING_DEPENDENCIES=true
+
+CCACHE_DIR='/mnt/ccache'
+if [ ! -f /mnt/ccache/ccache.conf ]
+then
+sudo mount --bind /home/jabiyeff/.ccache ${CCACHE_DIR}
+fi
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+export CCACHE_DIR=${CCACHE_DIR}
