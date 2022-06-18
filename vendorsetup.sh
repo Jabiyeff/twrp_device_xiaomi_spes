33d43d787414313d91b911e5240e85bb500eecc7
Jabiyeff <cebiyevanar@gmail.com>
2022-06-18 11:09:48 +0400

    spes: Update Magisk to 25.0 version
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -60,7 +60,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     export FOX_VERSION="R11.1_3"
 
 	# use magisk 24.3 for the magisk addon
-	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/ofrp/device/xiaomi/spes/addon/Magisk-v24.3.zip
+	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/ofrp/device/xiaomi/spes/addon/Magisk-v25.0.zip
 
 	# OTA
 	export OF_KEEP_DM_VERITY=1
