50f6ff34caabc95bb84310dad742c9c4d6b914b9
Jabiyeff <cebiyevanar@gmail.com>
2022-12-25 12:45:04 +0400

    spes: Update boot control HAL to V1.2
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -46,7 +46,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti.recovery
+    android.hardware.boot@1.2-impl-qti.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
